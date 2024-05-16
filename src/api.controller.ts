6bfc2fafc7708f3ee9d3907a0877e877ea7c3449
ahmed-said7 <engahmedsaid125@gmail.com>
2024-05-17 02:59:00 +0300

    commit

--- src/api.controller.ts
+++ src/api.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Req } from "@nestjs/common";
+import { Controller, Post, Req } from "@nestjs/common";
 import { Request } from "express";
 
 @Controller("paytab")
 export class paytabController {
+    @Post()
     logQuery(@Req() req:Request ){
         console.log(req.query);
     }
