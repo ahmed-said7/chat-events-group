2ce4c871973c088111f5ef3eb889835afe62d1fc
ahmed-said7 <engahmedsaid125@gmail.com>
2024-05-31 03:35:31 +0300

    otp

--- src/nodemailer/nodemailer.service.ts
+++ src/nodemailer/nodemailer.service.ts
@@ -41,6 +41,6 @@ export class mailerService {
         return this.transport.sendMail(opts);
     };
     resetCode(){
-        return String(Math.floor( 10000 + Math.random() * 90000));
+        return String(Math.floor( 1000 + Math.random() * 8000));
     };
 };
